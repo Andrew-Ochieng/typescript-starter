d5a1692a6b85210c7914bf93160fdb0f81df2fe4
Code-X <andrewomosh44@gmail.com>
2023-07-26 14:41:39 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// console.log('hello')
+var person = {
+    name: "Andrew",
+    age: 24
+};
+console.log(person);
---
+++ index.ts
@@ -0,0 +1,43 @@
+// console.log('hello')
+
+// let age: number;
+// age = 21;
+
+// console.log(age)
+
+// arrays
+// let students: string[]
+
+// students = ['Andrea', 'Phillip', 'Jason', 'Sylvia']
+
+// let newStudents: string[] 
+// newStudents = [...students, 'Maria']
+
+// console.log(newStudents)
+
+// let boolArr: boolean[]
+
+// boolArr = [true, false, false, true]
+
+// console.log(boolArr)
+
+
+
+
+
+// interface
+interface Person {
+    name: string,
+    age: number
+}
+
+let person: Person = {
+    name: "Andrew",
+    age: 24
+}
+
+console.log(person)
+
+
+
+
